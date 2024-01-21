b33c1645e84c20f1607653efc778034667fa20e3
Ina Ricci <ina_ricci@hotmail.com>
2024-01-21 14:06:44 -0800

    cleaned up code

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs")
 
