7e2328406379c96990af37aa9d2cc99df59cb1f7
Ina Ricci <ina_ricci@hotmail.com>
2024-01-22 05:04:11 -0800

    added code to display the username and render it in the header

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.json())
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser())
