6afa336c60415d53def6ca5f771fa351c220c336
Ina Ricci <ina_ricci@hotmail.com>
2024-01-29 05:42:06 -0800

    final project

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs")
 
 app.use(cookieSession({
